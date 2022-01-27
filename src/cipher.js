148a86a7e6a01adc0e11eec7e079160c43de8274
Lidianne Barbosa <lidiannerb@gmail.com>
2022-01-27 20:22:11 -0300

    primeiros passos no js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,42 @@
 const cipher = {
-  // ...
-};
 
+  encode: function (offset, text){
+
+    let textCoded = "";
+    
+    for (let i=0; i < text.length; i++){
+
+      let encrypt = text.charCodeAt(i);
+
+      if (encrypt >= 65 && encrypt <= 90){
+
+        /*textCoded += String.fromCharCode (((encrypt - 65 + offset %26) + 26) %26 + 65);*/
+
+        textCoded += String.fromCharCode (((encrypt - 65 + offset) % 26) + 65);
+      } 
+    
+    }   
+    return textCoded;
+  }
+
+}
 export default cipher;
+
+
+
+
+
+
+/*(palavra, desloc)=> {
+const tamPalavra = palavra.length;
+ 
+  for (let i = 0; i < tamPalavra; i++){
+    const codAsc = palavra.charCodeAt(i);
+    const cifrando = ((codAsc - 65 + desloc) % 26) + 65;
+
+  }
+  
+}*/
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+const btnEncrypt = document.getElementById('btnsend'); 
+
+function encryptingText (e){
+
+    e.preventDefault()
+
+    let offset = Number(document.getElementById("offset").value);
+
+    let string = document.getElementById("textencrypt").value;
+    
+    const encrypting = cipher.encode(offset, string);
+    
+    document.getElementById("answer").innerHTML = encrypting;
+
+}
+
+btnEncrypt.addEventListener('click', encryptingText)
+
+
+                     
+
+
