f1da11fe9103ddfc6f102cedbe65b5309fff1b6e
Lidianne Barbosa <lidiannerb@gmail.com>
2022-01-29 17:39:11 -0300

    Inclusão do decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,25 +1,38 @@
-const cipher = {
+const cipher = {  /* constante que se torna um objeto (pois contem um bloco entre chaves) o qual será utilizado no index.js  */
 
-  encode: function (offset, text){
+encode: function (offset, text){
 
     let textCoded = "";
     
     for (let i=0; i < text.length; i++){
-
       let encrypt = text.charCodeAt(i);
-
       if (encrypt >= 65 && encrypt <= 90){
-
-        /*textCoded += String.fromCharCode (((encrypt - 65 + offset %26) + 26) %26 + 65);*/
-
-        textCoded += String.fromCharCode (((encrypt - 65 + offset) % 26) + 65);
-      } 
-    
+        textCoded += String.fromCharCode(((encrypt - 65 + offset) % 26) + 65);
+      } else if (text.charCodeAt(i) == 32){
+        textCoded += text.charAt(i);
+      }
     }   
     return textCoded;
-  }
+  },
 
+decode: function (offset, text){
+
+    let textDecoded = "";
+    
+    for (let i=0; i < text.length; i++){
+      let encrypt = text.charCodeAt(i);
+      if (encrypt >= 65 && encrypt <= 90){
+        textDecoded += String.fromCharCode(((encrypt - 65 - offset) % 26) + 65);
+      } else if (text.charCodeAt(i) == 32){
+        textDecoded += text.charAt(i);
+      }
+    }   
+    return textDecoded;
+  }
 }
+
+
+
 export default cipher;
 
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
 import cipher from './cipher.js';
 
 
-const btnEncrypt = document.getElementById('btnsend'); 
+const btnEncrypt = document.getElementById('btnsend1'); 
 
-function encryptingText (e){
+function encryptingText (e){  
 
-    e.preventDefault()
-
+    e.preventDefault()  /* interrompe o recarregamento automatico da página*/
     let offset = Number(document.getElementById("offset").value);
-
-    let string = document.getElementById("textencrypt").value;
-    
-    const encrypting = cipher.encode(offset, string);
-    
-    document.getElementById("answer").innerHTML = encrypting;
-
+    let string = document.getElementById("textencrypt").value;    
+    const encrypting = cipher.encode(offset, string);    
+    document.getElementById("answer1").innerHTML = encrypting;
 }
 
 btnEncrypt.addEventListener('click', encryptingText)
 
+
+const btnDecrypt = document.getElementById('btnsend2'); 
+
+function decryptingText (e){  
+
+    e.preventDefault()  /* interrompe o recarregamento automatico da página*/
+    let offset = Number(document.getElementById("offset").value);
+    let string = document.getElementById("textdecrypt").value;    
+    const decrypting = cipher.decode(offset, string);    
+    document.getElementById("answer2").innerHTML = decrypting;
+}
+
+btnDecrypt.addEventListener('click', decryptingText)
 
                      
 
