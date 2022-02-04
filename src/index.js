c186f6437c82c883dc1aa4f713842aeed27f9e26
Lidianne Barbosa <lidiannerb@gmail.com>
2022-02-03 23:07:45 -0300

    correção do decode

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const btnEncrypt = document.getElementById('btnsend1');
 function encryptingText (e){  
 
     e.preventDefault()  /* previne o comportamento padrão do "click", de enviar dados  a páginrecarregandoa*/
-    let offset = Number(document.getElementById("offset").value);
-    let string = document.getElementById("textencrypt").value;    
+    const offset = Number(document.getElementById("offset").value);
+    const string = document.getElementById("textencrypt").value;    
     const encrypting = cipher.encode(offset, string);    
     document.getElementById("answer1").innerHTML = encrypting;
 }
@@ -19,8 +19,8 @@ const btnDecrypt = document.getElementById('btnsend2');
 function decryptingText (e){  
 
     e.preventDefault() 
-    let offset = Number(document.getElementById("offset").value);
-    let string = document.getElementById("textdecrypt").value;    
+    const offset = Number(document.getElementById("offset").value);
+    const string = document.getElementById("textdecrypt").value;    
     const decrypting = cipher.decode(offset, string);    
     document.getElementById("answer2").innerHTML = decrypting;
 }
