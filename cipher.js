0510e8d9a4ea1b81d8278dd34de69b85646163c5
Lidianne Barbosa <lidiannerb@gmail.com>
2022-02-07 13:27:47 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -2,37 +2,60 @@ const cipher = {
 
 encode: function (offset, text){
 
+    if (typeof offset != 'number' || (offset !=0 && offset%1 !=0)){
+      throw new TypeError();
+    }
+    if(typeof text != 'string'){
+      throw new TypeError();
+    }
+
   let textCoded = "";
     
-  for (let i=0; i < text.length; i++){
+  for (let i=0; i < text.length; i++){  
      let encrypt = text.charCodeAt(i);
-    if (encrypt >= 65 && encrypt <= 90){
-      textCoded += String.fromCharCode(((encrypt - 65 + offset) % 26) + 65);
-    }else if (encrypt >= 97 && encrypt <= 122){
-      textCoded += String.fromCharCode(((encrypt - 97 + offset) % 26) + 97);
-    }else if (encrypt == 32) {
-      textCoded += text.charAt(i);
+      
+      if (encrypt >= 65 && encrypt <=90){         
+        encrypt = ((encrypt - 65 + offset) % 26) + 65;
+      
+      } if (encrypt >= 97 && encrypt <= 122){
+        encrypt = ((encrypt - 97 + offset) % 26) + 97;
+
+      } else if (encrypt == 32) {
+        encrypt = encrypt + text.charAt(i);
+      } 
+      textCoded += String.fromCharCode(encrypt);
     }
-  }   
     return textCoded;
-  },
+},
 
 decode: function (offset, text){
+
+  if (typeof offset != 'number' || (offset !=0 && offset%1 !=0)){
+    throw new TypeError();
+  }
+  if(typeof text != 'string'){
+    throw new TypeError();
+  }
 
   let textDecoded = "";
       
   for (let i=0; i < text.length; i++){
+
     let decrypt = text.charCodeAt(i);
-     if (decrypt >= 65 && decrypt <= 90){
-       textDecoded += String.fromCharCode(((decrypt - 65 - offset) % 26) + 65);
-     }else if (decrypt >= 97 && decrypt <= 122){
-      textDecoded += String.fromCharCode(((decrypt - 97 - offset) % 26) + 97);
-     }else if(decrypt == 32) {
-       textDecoded += text.charAt(i);
-     }
+     
+      if (decrypt >= 65 && decrypt <= 90){
+        decrypt = ((decrypt - 90 - offset) % 26) + 90;
+
+      }if (decrypt >= 97 && decrypt <= 122){
+        decrypt = ((decrypt - 122 - offset) % 26) + 122;
+
+      }else if (decrypt == 32) {
+        decrypt = (decrypt + text.charAt(i));
+      }
+
+      textDecoded += String.fromCharCode(decrypt);
   }   
     return textDecoded;
   }
-}
-
+};
 export default cipher;
--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const btnEncrypt = document.getElementById('btnsend1');
 
 function encryptingText (e){  
 
-    e.preventDefault()  /* previne o comportamento padrão do "click", de enviar dados  a páginrecarregandoa*/
-    let offset = Number(document.getElementById("offset").value);
-    let string = document.getElementById("textencrypt").value;    
+    e.preventDefault()  
+    const offset = Number(document.getElementById("offset").value);
+    const string = document.getElementById("textencrypt").value;    
     const encrypting = cipher.encode(offset, string);    
     document.getElementById("answer1").innerHTML = encrypting;
 }
@@ -19,15 +19,15 @@ const btnDecrypt = document.getElementById('btnsend2');
 function decryptingText (e){  
 
     e.preventDefault() 
-    let offset = Number(document.getElementById("offset").value);
-    let string = document.getElementById("textdecrypt").value;    
+    const offset = Number(document.getElementById("offset").value);
+    const string = document.getElementById("textdecrypt").value;    
     const decrypting = cipher.decode(offset, string);    
     document.getElementById("answer2").innerHTML = decrypting;
 }
 btnDecrypt.addEventListener('click', decryptingText)
 
 
-/*Abaixo estão as funções para abrir e fechar o display com as instruções de uso*/ 
+
 
 
 const btnInstruct = document.getElementById("btnModal");
@@ -35,15 +35,16 @@ const btnInstruct = document.getElementById("btnModal");
 function instruct () {
 
     let modal = document.querySelector('.modal')
-    modal.style.display = 'block'; // mudando o display de forma dinâmica (js)
+    modal.style.display = 'block';  
 }
 btnInstruct.addEventListener('click', instruct)
+
 
 const modalOff = document.getElementById("btnFecharModal");
 
 function instructOff () {
 
     let modal = document.querySelector('.modal')
-    modal.style.display = 'none'; // mudando o display de forma dinâmica (js)
+    modal.style.display = 'none'; 
 }
 modalOff.addEventListener('click', instructOff)
